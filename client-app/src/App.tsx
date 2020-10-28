8ca9f9da0f7676dfa6d744c16beffd475904a0b0
Samuel Hilson <samrhilson@gmail.com>
2020-10-28 09:41:56 -0500

    Add semantic ui framework

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,37 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Header, Icon, List } from 'semantic-ui-react';
 import './App.css';
 import Axios from 'axios';
 
 class App extends Component {
   state = {
-    values: []
-  }
+    values: [],
+  };
 
   componentDidMount() {
-    Axios.get('http://localhost:5000/api/values')
-      .then((response) => {
-        console.log(response);
-        this.setState({
-          values: response.data
-        })
-      })
+    Axios.get('http://localhost:5000/api/values').then((response) => {
+      this.setState({
+        values: response.data,
+      });
+    });
     this.setState({
-      values: [{id: 1, name: 'Value 101'}, {id: 2, name: 'Value 102'}]
-    })
+      values: [
+        { id: 1, name: 'Value 101' },
+        { id: 2, name: 'Value 102' },
+      ],
+    });
   }
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <ul>
-            {this.state.values.map((value: any) =>(
-              <li key={value.id}>{value.name}</li>
-            ))}
-          </ul>
-        </header>
+      <div>
+        <Header as="h2">
+          <Icon name="users" />
+          <Header.Content>Reactivities</Header.Content>
+        </Header>
+        <List>
+          {this.state.values.map((value: any) => (
+            <List.Item key={value.id}>{value.name}</List.Item>
+          ))}
+        </List>
       </div>
     );
   }
